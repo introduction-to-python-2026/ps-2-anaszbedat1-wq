523dbb6bb6c35685fbdb0e7ccbeeda00a1575902
anaszbedat1-wq <anaszbedat1@gmail.com>
2025-11-03 23:16:44 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,3 +1,8 @@
+def find_max_number(num1, num2, num3):
+    return max(num1, num2, num3)
+
+def find_mean(num1, num2, num3):
+    return (num1 + num2 + num3) / 3
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
